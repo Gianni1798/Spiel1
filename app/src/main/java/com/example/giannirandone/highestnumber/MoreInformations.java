9a083093ba1fc3266530fefd3cbc8ddadf8444b1
randonegianni <info.randonegianni@gmail.com>
2019-06-09 23:37:02 +0200

    More Information - Button wurde hinzugefügt.

---
+++ app/src/main/java/com/example/giannirandone/highestnumber/MoreInformations.java
@@ -0,0 +1,22 @@
+package com.example.giannirandone.highestnumber;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MoreInformations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_informations);
+
+
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/giannirandone/highestnumber/Settings.java
+++ app/src/main/java/com/example/giannirandone/highestnumber/Settings.java
@@ -1,12 +1,14 @@
 package com.example.giannirandone.highestnumber;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -17,12 +19,18 @@ public class Settings extends AppCompatActivity {
 
     TextView settingsTextViewHighscoreAusgabe;
     Switch switch_Color;
+    Button moreInformation;
     //AdView adView_Settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button moreInformations = (Button) findViewById(R.id.btn_moreInformations);
+        moreInformations.setOnClickListener(v -> {
+            startActivity(new Intent(Settings.this, MoreInformations.class ));
+        });
 
         //adView_Settings = (AdView) findViewById(R.id.adView_Settings);
 
